78271279cf9a3bcf3353cc6255490b79cc3b9c52
TaiohY <taioh.yokoyama@etu.unistra.fr>
2022-07-12 08:35:30 +0200

    .

--- Script/Load_explore.R
+++ Script/Load_explore.R
@@ -1,1 +1,23 @@
-DB_nat2020 <- read.csv(file="./BDD/nat2020.csv")
+# Load_explore 
+# Taioh Y 11.07.22
+
+# Charger la base de données et explorer son contenu
+
+# Packages ####
+library(tidyverse)
+
+# Database ####
+DB_dpt2020 <- read.csv(file="./BDD/dpt2020.csv", sep = ";")
+
+# Objets ####
+tbl_dpt2020 <- DB_dpt2020 %>%
+  mutate(sexe = as.factor(sexe),
+         preusuel = as.factor(preusuel),
+         annais = as.numeric(annais),
+         dpt = as.factor(dpt),
+         nombre = as.numeric(nombre))
+
+# Scripts ####
+
+names(tbl_dpt2020)
+length(levels(tbl_dpt2020$preusuel))
